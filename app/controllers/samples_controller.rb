c1a03959b0cf132a20bd605551ea9e057af871ba
mygkw <miyagaki@wyrd.co.jp>
2022-03-10 18:22:35 +0900

    処理を少し改良

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -5,32 +5,20 @@ class SamplesController < ApplicationController
 
   # GET /samples
   def index
-     render json: { status: "ok"}
-  end
-
-  # GET /samples/1
-  def show
-    render json: { status: "ok"}
+     render json: { val: "今のデータだよ" }
   end
 
   # POST /samples
   def create
-    render json: { status: "ok"}
-  end
-
-  # PATCH/PUT /samples/1
-  def update
-    render json: { status: "ok"}
-  end
-
-  # DELETE /samples/1
-  def destroy
-    render json: { status: "ok"}
+    # "フロントエンドでPOSTできること"までが今回の確認観点なので、ここでは何もしない
+    render json: {}
   end
 
   private
 
   def basic_auth
+    sleep 1 # フロントエンドのロード確認用にウェイト
+
     authenticate_or_request_with_http_basic do |username, password|
       username == "hoge" && password == "fuga"
     end
